989a459b17a5725f03ca30724c796b8382d3ac2d
Piotr Lalak <56452426+piotrlalak@users.noreply.github.com>
2023-09-25 18:02:16 +0100

    Add files via upload

---
+++ KeyToObjects.jsx
@@ -0,0 +1,266 @@
+﻿$.writeln('\n-------------- KeyToObjects.jsx --------------\nApplying scale & opacity by black value...\n');
+userInput();
+if(doSomething === true){
+	current_selection = app.activeDocument.selection
+	if (current_selection.length > 1){
+		
+		units = 72/25.4;
+		objectsMap = app.activeDocument.selection[0]
+		halftone = app.activeDocument.selection[1]
+		mappedObjectsGroup = app.activeDocument.activeLayer.groupItems.add();
+		
+		keyToObjects(objectsMap,halftone)
+	}
+}
+$.writeln('\n--------------      Finished       --------------');
+//-----------------------------------------------------------------------------------------------------------------------------------
+function keyToObjects(objectMap_group,halftone_group){
+	$.writeln("keyToObjects - halftone length: " + halftone_group.pageItems.length + ", objects map length: " + objectsMap.pageItems.length)
+	
+	for(a=0;a<halftone_group.pageItems.length;a++){
+		
+		c_h_item = halftone_group.pageItems[a]
+		c_XY = boundsToXY(c_h_item.geometricBounds)
+		c_h_item_size = visibleObjectBounds(c_h_item)
+		c_h_item_width = -(c_h_item_size[0]-c_h_item_size[2])/units
+		
+		//--------------------------
+
+		if(c_h_item.fillColor.typename === 'CMYKColor'){ 
+			c_h_item_key = c_h_item.fillColor.black
+		}else if(c_h_item.fillColor.typename === 'GrayColor'){
+			c_h_item_key= c_h_item.fillColor.gray
+		}else{
+			continue
+		}
+	
+		c_h_item_key = 100-c_h_item_key
+	
+		//--------------------------
+
+		c_multiplier = 100 / (objectsMap.pageItems.length-1)
+		c_h_index = c_h_item_key/c_multiplier
+		c_h_index = c_h_index.toFixed(0)
+		c_h_index = parseInt(c_h_index)
+				
+		//--------------------------
+		
+		mapped_object = objectsMap.pageItems[c_h_index].duplicate(mappedObjectsGroup, ElementPlacement.PLACEATBEGINNING);
+		m_XY = boundsToXY(mapped_object.geometricBounds)
+		m_size = visibleObjectBounds(mapped_object)
+		m_width = -(m_size[0]-m_size[2])/units
+		
+		//--------------------------
+
+		c_scale = c_h_item_width / m_width * 100
+		rescaleArtwork (mapped_object,c_scale,true)
+		mapped_object.translate(c_XY[0]-m_XY[0],m_XY[1]-c_XY[1],true,true,true,true)
+
+	}
+
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+function findSizeExtremes(selection){
+	if(selection.length>0){
+		//$.writeln('Looking for size extremes....')
+		//--------------------------------------------------------
+		sWidth = boundsToSizes(visibleObjectBounds(selection[0]))[0];
+		sHeight = boundsToSizes(visibleObjectBounds(selection[0]))[1];
+		lWidth = boundsToSizes(visibleObjectBounds(selection[0]))[0];
+		lHeight = boundsToSizes(visibleObjectBounds(selection[0]))[1];
+		sWidthObject = selection[0];
+		sHeightObject = selection[0];
+		lWidthObject = selection[0];
+		lHeightObject = selection[0];
+		//--------------------------------------------------------
+		for(a=0;a<selection.length;a++){
+			cWidth = boundsToSizes(visibleObjectBounds(selection[a]))[0];
+			cHeight = boundsToSizes(visibleObjectBounds(selection[a]))[1];
+			if(cWidth<sWidth){
+				sWidth = cWidth;
+				sWidthObject = selection[a];
+			}
+			if(cWidth>lWidth){
+				lWidth = cWidth;
+				lWidthObject = selection[a];
+			}
+			if(cHeight<sHeight){
+				sHeight = cHeight;
+				sHeightObject = selection[a];
+			}
+			if(cHeight>lHeight){
+				lHeight = cHeight;
+				lHeightObject = selection[a];
+			}
+		}
+		//--------------------------------------------------------
+		/*
+		app.activeDocument.selection = null;
+		sWidthObject.selected = true;
+		sHeightObject.selected = true;
+		lWidthObject.selected = true;
+		lHeightObject.selected = true;
+		*/
+		//--------------------------------------------------------
+		/*$.writeln('-------------------------------------\nSmallest:\n  Width: '
+				+ sWidth + '\n  Height: ' + sHeight + '\nLargest:\n  Width: ' + lWidth + '\n  Height: ' + lHeight);*/
+	}else{
+		//do nothing
+	}
+}
+//----------------------------------------------------
+function rescaleArtwork (item,scale,stroke){
+	if (stroke == true){
+		var strokeScale = scale;
+		}else{
+		var strokeScale = 0.0;
+		}
+	item.resize(
+					scale,
+					scale,
+					true /*changes position - always true - required to change size*/ ,
+					true /* changes pattern */ ,
+					true /* changes gradient */ ,
+					true /* changes stroke patterns */ , 
+					strokeScale /* changes stroke width */ ,
+					Transformation.CENTER
+					);
+}
+//----------------------------------------------------
+function boundsToXY(bounds){
+	xy = [bounds[0] + ((bounds[2] - bounds[0])/2),
+			-bounds[1] - ((bounds[3] - bounds[1])/2)
+			]
+	return xy;
+}
+//----------------------------------------------------
+function visibleObjectBounds(object){
+	var objectBounds = [];
+	switch(object.typename){
+		case 'PathItem':
+			objectBounds = object.geometricBounds;
+			break;
+		case 'CompoundPathItem':
+			objectBounds = object.geometricBounds;
+			break;
+		case 'GroupItem':
+			if(object.clipped === true){
+				objectBounds = object.pageItems[0].geometricBounds;
+			}else{
+				objectBounds = object.pageItems[0].geometricBounds;
+				for(var g=1;g<object.pageItems.length;g++){
+					if(object.pageItems[g].typename !== 'GroupItem'){
+						objectBounds = replaceBiggerBounds(object.pageItems[g].geometricBounds,objectBounds);
+					}else{
+						subBounds = visibleObjectBounds(object.pageItems[g]);
+						objectBounds = replaceBiggerBounds(subBounds,objectBounds);
+					}
+				}
+			}
+			break;
+		default:
+			objectBounds = object.geometricBounds;
+			break;
+	}
+	return objectBounds;
+}
+//----------------------------------------------------
+function replaceBiggerBounds(newBounds,oldBounds){//replaces bounds if they are bigger
+	//--------------------------------------------------------
+	if(newBounds[0] < oldBounds[0]){
+		oldBounds[0] = newBounds[0];
+	}
+	//--------------------------------------------------------
+	if(newBounds[1] > oldBounds[1]){
+		oldBounds[1] = newBounds[1];
+	}
+	//--------------------------------------------------------
+	if(newBounds[2] > oldBounds[2]){
+		oldBounds[2] = newBounds[2];
+	}
+	//--------------------------------------------------------
+	if(newBounds[3] < oldBounds[3]){
+		oldBounds[3] = newBounds[3];
+	}
+	return oldBounds;
+}
+//----------------------------------------------------
+function neg2pos(value){
+	if(value < 0){
+		newValue = value / -1
+	}else{
+		newValue = value;
+	}
+	return newValue;
+}
+//----------------------------------------------------
+function cmykValue(v){
+	if (v < 0){
+		v = 0;
+	}
+	if (v>100){
+		v=100;
+	}
+	return v;
+}
+//----------------------------------------------------
+function customColour(c,m,y,k){
+	var customCMYK = new CMYKColor();
+	customCMYK.cyan = cmykValue(c);
+	customCMYK.magenta = cmykValue(m);
+	customCMYK.yellow = cmykValue(y);
+	customCMYK.black = cmykValue(k);
+	return customCMYK;
+}
+//----------------------------------------------------
+function crosshair(x,y,s,l){ //development only
+	
+	//--------------------------------------------------------
+	scale = 1;
+	//--------------------------------------------------------
+	crosshairGroup = l.groupItems.add();
+	crosshairGroup.name = 'Crosshair'
+	//--------------------------------------------------------
+	line = crosshairGroup.pathItems.add();
+	line.setEntirePath([[x,-y+((s/2)*units)],[x,-y-((s/2)*units)]])
+	line.filled = false;
+	line.stroked = true;
+	line.strokeColor = customColour(100,0,0,0);
+	line.strokeWidth = scale * units / 4;
+	line.strokeDashes = [];
+	//--------------------------------------------------------
+	line.duplicate(crosshairGroup,ElementPlacement.PLACEATEND)
+	crosshairGroup.pageItems[0].rotate(90,true,true,true,true,Transformation.CENTER);
+	//--------------------------------------------------------
+	return crosshairGroup;
+}
+//----------------------------------------------------
+function userInput(){
+	//----------------------------------------------------
+	doSomething = false;
+	//----------------------------------------------------
+	var window = new Window ('dialog',"KeyToObjects");
+	mainGroup = window.add('group');
+	mainGroup.orientation = 'column'
+	mainGroup.alignChildren = 'center'
+	//----------------------------------------------------
+	infoGroup = mainGroup.add('group');
+	infoGroup.orientation = 'column'
+	infoGroup.alignChildren = 'center'
+		info1 = infoGroup.add ("statictext", undefined,'Script will map object from 1st group');
+		info1 = infoGroup.add ("statictext", undefined,'to each path in 2nd group based on key value.');
+		info3 = infoGroup.add ("statictext", undefined,'Press OK to continue');
+	//----------------------------------------------------
+	divider1 = mainGroup.add('panel',([undefined,undefined,150,undefined]),undefined,{borderStyle:'white'});
+	//----------------------------------------------------
+	var okButton = mainGroup.add ("button", undefined, "OK");
+	okButton.onClick = function (){
+		doSomething = true;
+		window.close();
+	}
+	//----------------------------------------------------
+	window.show();
+	//----------------------------------------------------
+	return doSomething;
+}
---
+++ PartcodesBoxes.jsx
@@ -0,0 +1,351 @@
+﻿//----------------------------------------------------
+units = 72/25.4
+
+userInput()
+if(doSomething === true){
+	if(selection.length===1){
+		partcodesBoxes()
+	}
+}else{
+	//donothing
+}
+
+//----------------------------------------------------
+function partcodesBoxes(){
+	
+	//----------------------------- INIT VARS
+
+	active_lay = app.activeDocument.activeLayer
+	active_art = app.activeDocument.artboards[app.activeDocument.artboards.getActiveArtboardIndex()]
+	
+	xOrigin = selection[0].geometricBounds[0]
+	yOrigin = selection[0].geometricBounds[1]
+	
+	box_width = 15 * units
+	box_height = 5 * units
+	
+	//----------------------------- PREFLIGHT CHECKS
+	textframe_length = Boolean(selection.length === 1)
+	headerMatch = checkHeader(selection[0].contents);
+	if(textframe_length == true && headerMatch == true){
+		partcodesArray = partcodeCSVArray(selection[0].contents);
+		newBoxesGroup = active_lay.groupItems.add();
+		newBoxesGroup.name = 'PartcodesBoxes'
+	}else{
+		partcodesArray = null
+	}
+	//----------------------------- MAIN LOOP
+	columnsNo = Math.ceil((Math.sqrt(partcodesArray.length)))
+	cCol = 0
+	cRow = 0
+	hOffset = box_width+ (2*units)
+	vOffset = box_height+ (2*units)
+	chOffset = 0
+	cvOffset = 0
+	//------------------------------------------------
+	for(a=0;a<partcodesArray.length;a++){
+		chOffset = hOffset * cCol
+		cvOffset = vOffset * cRow
+		if(removePrefix == true){
+			new_partcode_string = stringReplacement(partcodesArray[a].partcode,prefixInput,'')
+		}else{
+			new_partcode_string = partcodesArray[a].partcode
+		}
+		newBox(xOrigin+chOffset,yOrigin-cvOffset,newBoxesGroup,partcodesArray[a].production,new_partcode_string)
+		if(cCol==columnsNo){
+			cCol=0
+			cRow++
+		}else{
+			cCol++
+		}
+	}
+}
+//----------------------------------------------------
+function newBox(x,y,l,t,d_text){
+	newBoxGroup = l.groupItems.add();
+	newBoxGroup.name = d_text
+	//----------------------------------------------------
+	
+	box_width = 15 * units
+	box_height = 5 * units
+	line_lenght = 15 * units
+	text_offset = 0.383 * units
+	stroke_width = 0.35 * units
+	
+	type_die_colour = customColour(0,100,0,0)
+	type_kiss_colour = customColour(100,0,0,0)
+	type_other_colour = customColour(100,0,100,0)
+	white_colour = customColour(0,0,0,0)
+	
+	//---------------------------------------------------- TYPE BOX
+
+	base_line = newBoxGroup.pathItems.add(x,y,box_width,box_height,false,false);
+	base_line.setEntirePath([[x+box_width/2,y-box_height/2],[x+box_width/2,y-box_height/2-line_lenght]]);
+	base_line.closed = false;
+	base_line.filled = false;
+	base_line.stroked = true;
+	
+	base_line.strokeWidth = stroke_width;
+	base_line.strokeDashes = [];
+	
+	base_box = newBoxGroup.pathItems.add(x,y,box_width,box_height,false,false);
+	base_box.setEntirePath([[x,y],[x+box_width,y],[x+box_width,y-box_height],[x,y-box_height]]);
+	base_box.closed = true;
+	base_box.filled = true;
+	base_box.stroked = false;
+
+	switch(t){
+		case 'PRINT':
+			base_box.fillColor = type_die_colour;
+			base_line.strokeColor = type_die_colour;
+			break;
+		case 'Print':
+			base_box.fillColor = type_die_colour;
+			base_line.strokeColor = type_die_colour;
+			break;
+		case 'CUT':
+			base_box.fillColor = type_kiss_colour;
+			base_line.strokeColor = type_kiss_colour;
+			break;
+		case 'Cut':
+			base_box.fillColor = type_kiss_colour;
+			base_line.strokeColor = type_kiss_colour;
+			break;
+		default:
+			base_box.fillColor = type_other_colour;
+			base_line.strokeColor = type_other_colour;
+	}
+	
+	box_TextFrame = newBoxGroup.textFrames.add();
+	box_TextFrame.contents = d_text;
+	box_TextFrame.top = y-text_offset
+	box_TextFrame.left = x+(box_width/2)
+	box_TextFrame.textRange.characterAttributes.size = 12;
+	box_TextFrame.textRange.characterAttributes.fillColor = white_colour;
+	box_TextFrame.textRange.paragraphAttributes.justification = Justification.CENTER;
+	box_TextFrame.textRange.characterAttributes.textFont = app.textFonts.getByName('MyriadPro-Semibold');
+	
+
+}
+//------------------------------------------
+function checkHeader(csv){
+	//------------------------------------------
+	csvArray = csv.split("\r");
+	activeHeaderColumns = csvArray[0];
+	baseHeaderColumns = 'PARTCODE,DESCRIPTION,LOCATION,QUANTITY,PRODUCTION';
+	//------------------------------------------
+	if(activeHeaderColumns===baseHeaderColumns){
+		headerMatch = true;
+	}else{
+		headerMatch = false;
+	}
+	//------------------------------------------
+	return headerMatch;
+}
+//------------------------------------------
+function partcodeCSVArray(csv){
+	//------------------------------------------
+	tempArray = csv.split('\r');
+	tempArray = tempArray.toString();
+	tempArray = tempArray.split(',')
+	partcodesArray = null;
+	//------------------------------------------
+	partcodesArray = [];
+	//------------------------------------------
+	checkArrayHeader(tempArray)
+	counter = 0;
+	//------------------------------------------
+	if(arrayHeaderMatch === true){
+		for(f=5;f<tempArray.length-5;f++){
+			partcodesArray[counter] = {partcode:tempArray[f], description:tempArray[(f+1)], part:tempArray[(f+2)], quantity:tempArray[(f+3)], production:tempArray[(f+4)]};
+			f+=4
+			counter++;
+		}
+	}else{
+		partcodesArray = null;
+	}
+	//------------------------------------------
+	return partcodesArray;
+}
+//------------------------------------------
+function checkArrayHeader(array){
+	//------------------------------------------
+	testArrayHeaderString = array[0] + ',' + array[1] + ',' + array[2] + ',' + array[3] + ',' + array[4];
+	//------------------------------------------
+	if(testArrayHeaderString===baseHeaderColumns){
+		arrayHeaderMatch = true;
+	}else{
+		arrayHeaderMatch = false;
+	}
+	//------------------------------------------
+	return arrayHeaderMatch;
+}
+//----------------------------------------------------
+function boundsToXY(bounds){
+	xy = [bounds[0] + ((bounds[2] - bounds[0])/2),
+			-bounds[1] - ((bounds[3] - bounds[1])/2)
+			]
+	return xy;
+}
+//----------------------------------------------------
+function cmykValue(v){
+	if (v < 0){
+		v = 0;
+	}
+	if (v>100){
+		v=100;
+	}
+	return v;
+}
+//----------------------------------------------------
+function customColour(c,m,y,k){
+	var customCMYK = new CMYKColor();
+	customCMYK.cyan = cmykValue(c);
+	customCMYK.magenta = cmykValue(m);
+	customCMYK.yellow = cmykValue(y);
+	customCMYK.black = cmykValue(k);
+	return customCMYK;
+}
+//----------------------------------------------------
+function blackColour(){
+	var blackCMYK = new CMYKColor();
+	blackCMYK.cyan = 0;
+	blackCMYK.magenta = 0;
+	blackCMYK.yellow = 0;
+	blackCMYK.black = 100;
+	return blackCMYK;
+}
+//--------------------------------------------------------
+function crosshair(x,y,s,l){ //development only
+	
+	//--------------------------------------------------------
+	scale = 1;
+	//--------------------------------------------------------
+	crosshairGroup = l.groupItems.add();
+	crosshairGroup.name = 'Crosshair'
+	//--------------------------------------------------------
+	line = crosshairGroup.pathItems.add();
+	line.setEntirePath([[x,-y+((s/2)*units)],[x,-y-((s/2)*units)]])
+	line.filled = false;
+	line.stroked = true;
+	line.strokeColor = blackColour();
+	line.strokeWidth = scale * units / 4;
+	line.strokeDashes = [];
+	//--------------------------------------------------------
+	line.duplicate(crosshairGroup,ElementPlacement.PLACEATEND)
+	crosshairGroup.pageItems[0].rotate(90,true,true,true,true,Transformation.CENTER);
+	//--------------------------------------------------------
+	return crosshairGroup;
+}
+//--------------------------------------------------------
+function stringReplacement(a,b,c){//a-string, b- target, c-replacement
+	$.writeln('--------------->  stringReplacement')
+	//---------------------------------
+	originalString = a
+	replacedString = '';
+	//---------------------------------
+	var a = a.toString();
+	var b = b.toString();
+	var c = c.toString();
+	//---------------------------------
+	var a = a.split('');
+	var b = b.split('');
+	//---------------------------------
+	loopLength = a.length;
+	if(b.length>loopLength){
+		loopLength = b.length;
+	}
+	if(c.length>loopLength){
+		loopLength = c.length;
+	}
+	//---------------------------------
+	matchedString = false;
+	//---------------------------------
+	for(i=0;i<loopLength;i++){
+		//---------------------------------
+		if(a[i] === b[0]){
+			for(ii=0;ii<b.length;ii++){
+				if(a[i+ii] !== b[ii]){
+					break;
+				}
+				//---------------------------------
+				if(ii>=b.length-1){
+					i = i + b.length;
+					if(c === undefined){
+						continue
+					}else{
+						replacedString += c;
+					}
+				}
+				//---------------------------------
+			}
+		}
+		//---------------------------------
+		if(matchedString === false && i < loopLength){
+			if(c === undefined){
+				continue
+			}else{
+				replacedString += a[i];
+			}
+		}
+		//---------------------------------
+	}
+	//---------------------------------
+	if(originalString === ''){
+		replacedString = originalString
+	}
+	//---------------------------------
+	return replacedString;
+}
+//--------------------------------------------------------
+function userInput(){
+	//----------------------------------------------------
+	var w = new Window ('dialog',"PartcodesBoxes");
+	var mainGroup = w.add ('group');
+	mainGroup.orientation = 'column'
+	infoGroup = mainGroup.add('group');
+		infoGroup.orientation = 'column'
+		infoGroup.alignChildren = 'center'
+		info1 = infoGroup.add ("statictext", undefined,'Script will create partcodes boxes');
+		info2 = infoGroup.add ("statictext", undefined,'from selected csv.');
+	divider1 = mainGroup.add('panel',([undefined,undefined,120,undefined]),undefined,{borderStyle:'white'});
+	var prefixGroup = mainGroup.add ('group');
+	prefixGroup.orientation = 'column'
+	prefixGroup.alignChildren = 'center'
+	prefixCheck = prefixGroup.add ("checkbox", undefined, 'Remove prefix');
+		var prefixInputGroup = prefixGroup.add ('group');
+		prefixInputGroup.orientation = 'row'
+		prefixInputGroup.alignChildren = 'center'
+		prefixStatic = prefixInputGroup.add ("statictext", undefined, 'Prefix: ');
+		prefixInputEdit = prefixInputGroup.add ("edittext", ([undefined,undefined,100,21]), 'AST');
+		prefixInputGroup.enabled = false
+	divider2 = mainGroup.add('panel',([undefined,undefined,120,undefined]),undefined,{borderStyle:'white'});
+	//---------------------------------------------------- 
+	doSomething = false;
+	//----------------------------------------------------
+	var okButton = mainGroup.add ("button", undefined, "Ok");
+	okButton.onClick = function (){
+		doSomething = true;
+		w.close();
+	}
+	//----------------------------------------------------
+	prefixCheck.onClick = function (){
+		if(prefixInputGroup.enabled === true){
+			prefixInputGroup.enabled = false;
+		}else{
+			prefixInputGroup.enabled = true;
+		}
+	}
+	//----------------------------------------------------
+	w.show();
+	//----------------------------------------------------
+	if(prefixCheck.value === true){
+		removePrefix = true
+	}else{
+		removePrefix = false
+	}
+	//nameSize = parseFloat(nameSizeInput.text)
+	//----------------------------------------------------
+	prefixInput = prefixInputEdit.text
+	return doSomething,removePrefix,prefixInput;//,nameSize;
+}
